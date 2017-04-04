c1abd95a6b741df269592f0c5a4ee3b5a4706420
Eric Goettl <eric.l.goettl@gmail.com>
2017-04-03 19:32:24 -0500

    Initial Models
    Start to adding usable models to record person info and measurements.

---
+++ ExerciseTracking/ExerciseTracking/Models/BodyMeasurement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExerciseTracking.Models
+{
+    public class BodyMeasurement
+    {
+        [Key]
+        public int measurementNum { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dateOfMeasurement { get; set; }
+        [Required]
+        [Range(1,800)]
+        public double bodyWeight { get; set; }
+        [Required]
+        [Range(1, 108)]
+        public double heightInches { get; set; }
+        [Range(1, 120)]
+        public double waistCircumInches { get; set; }
+        [Range(1, 120)]
+        public double upperLegCircumInches { get; set; }
+
+    }
+}
---
+++ ExerciseTracking/ExerciseTracking/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExerciseTracking.Models
+{
+    public class Person
+    {
+        [Key]
+        public int personalID { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 1, ErrorMessage ="Must be between 1 and 25 characters.")]
+        public string FName { get; set; }
+        [StringLength(4, MinimumLength = 1)]
+        public string MInitial { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 1, ErrorMessage = "Must be between 1 and 25 characters.")]
+        public string LName { get; set; }
+    }
+}
